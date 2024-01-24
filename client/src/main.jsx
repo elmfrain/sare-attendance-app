38b0bee011fc17f050b3dd9e21d2d73df07e25a6
elmfrain <elmfer10@gmail.com>
2024-01-23 22:33:34 -0600

    use scss styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // Import fontawesome icons
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 // Import custom styling
-import './index.css'
+import './index.scss'
 
 import App from './App.jsx'
 
