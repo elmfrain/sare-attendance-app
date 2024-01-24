59e61ee6c6d9e075565d2d30abfbc2d436b12052
elmfrain <elmfer10@gmail.com>
2024-01-24 02:41:04 -0600

    create main server file

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+async function runServer() {
+  await apolloServer.start();
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  app.use('/graphql', expressMiddleware(apolloServer, {
+    context: authMiddleware
+  }));
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+      console.log(`Use GraphQL at http://<host>:${PORT}/graphql`);
+    });
+  });
+}
+
+runServer();
