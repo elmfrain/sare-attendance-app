078d136f8af101400c0b326e791cb880fd769d3d
elmfrain <elmfer10@gmail.com>
2024-01-24 04:27:11 -0600

    create login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,25 @@
+const { Admin, Attendance, Meeting, Member } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
 const resolvers = {
   Query: {
 
   },
 
   Mutation: {
+    login: async (parent, { username, password }) => {
+      const admin = await Admin.findOne({ username });
 
+      if(!admin)
+        throw AuthenticationError.message = "wtf";
+
+      const correctPassword = await admin.isCorrectPassword(password);
+      if(!correctPassword)
+        throw AuthenticationError;
+
+      const token = signToken(admin);
+      return { token, admin }
+    }
   }
 };
 
