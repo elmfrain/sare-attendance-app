6c0193defd7aed4dc34d0774a4146e385b7a7ab7
elmfrain <elmfer10@gmail.com>
2024-01-24 04:26:04 -0600

    signToken to take in admin as param

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,8 +28,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function(username, sareID, _id) {
-    const payload = { username, sareID, _id };
+  signToken: function({username,  _id}) {
+    const payload = { username, _id };
     return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: expiration });
   }
 }
