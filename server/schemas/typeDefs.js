1db04d194a2f5ac0771e1a9ba99053ad791ebf0d
elmfrain <elmfer10@gmail.com>
2024-01-24 04:26:57 -0600

    add types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,61 @@
 const typeDefs = `
+  type Meeting {
+    _id: ID!,
+    title: String!
+    at: String
+    date: String!
+    type: String!
+    fromTime: String!
+    toTime: String!
+    teams: [String]!
+  }
+
+  type EmergencyContact {
+    name: String!
+    relationship: String
+    phone: String!
+  }
+
+  type Member {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    joinDate: String!
+    sareID: Int!
+    email: String!
+    phone: String
+    rank: String!
+    role: String
+    emergencyContact: EmergencyContact
+  }
+
+  type Attendance {
+    _id: ID!
+    member: Member!
+    meeting: Meeting!
+    joinTime: String!
+    leaveTime: String
+  }
+
+  type Admin {
+    _id: ID!
+    username: String!
+    password: String!
+    sareID: Int
+    member: Member
+  }
+
+  type Auth {
+    token: ID!
+    admin: Admin!
+  }
+
   type Query {
     _dummy: String
   }
 
   type Mutation {
-    _dummy: String
+    login(username: String!, password: String!): Auth
   }
 `;
 
