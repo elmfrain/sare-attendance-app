c1ea84c424e8f4177bc8f171dde7c5e94da3a2cd
elmfrain <elmfer10@gmail.com>
2024-01-24 02:40:52 -0600

    jwt middleware util

---
+++ server/utils/auth.js
@@ -0,0 +1,35 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config()
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED'
+    }
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if(req.headers.authorization)
+      token = token.split(' ').pop().trim();
+
+    if(!token)
+      return req;
+
+    try {
+      const { data } = jwt.verify(token, process.env.JWT_SECRET, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid Token')
+    }
+
+    return req;
+  },
+  signToken: function(username, sareID, _id) {
+    const payload = { username, sareID, _id };
+    return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: expiration });
+  }
+}
