a26e133082e84111f590b10ac1f53db1305debf9
elmfrain <elmfer10@gmail.com>
2024-01-23 22:33:15 -0600

    fixed layout of login page

--- client/src/pages/AdminSignin.jsx
+++ client/src/pages/AdminSignin.jsx
@@ -5,13 +5,13 @@ export default function AdminSignin({theme, setTheme}) {
   return (
     <div className="d-flex flex-column vh-100 bg-dark">
       <NavigationBar theme={theme} setTheme={setTheme} />
-      <main className="container flex-grow-1 d-flex align-items-center">
-        <div className="d-flex flex-grow-1">
-          <div className="flex-grow-1 d-flex justify-content-center">
-            <div id="logo-display-area" className="h-100" />
-          </div>
+      <main className="container d-flex flex-column flex-grow-1">
+        <div className="flex-grow-1" />
+        <div className="d-flex justify-content-evenly">
+          <div id="logo-display-area" className="h-100" />
           <LoginForm />
         </div>
+        <div className="flex-grow-2" />
       </main>
     </div>    
   );
