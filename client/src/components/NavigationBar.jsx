19326dc3c01cf8c152811374bec531e9fe368d86
elmfrain <elmfer10@gmail.com>
2024-01-23 22:34:54 -0600

    make nav bar properly collapse

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -6,12 +6,16 @@ export default function NavigationBar({theme, setTheme}) {
 
   return (
     currentPage != "/" ? (
-    <nav className="navbar navbar-expand-md" data-bs-theme="dark" id="main-nav-bar">
+    <nav className="navbar navbar-expand-md bg-dark box-shadow" data-bs-theme="dark" id="main-nav-bar">
       <div className="container-fluid d-flex justify-content-center">
         <a className="navbar-brand d-flex justify-content-center align-items-center gap-3" href="#">
           <img src="/assets/images/logo/64.png"/>
           Attendance Portal
         </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+          {/* <span class="navbar-toggler-icon"></span> */}
+          <i className="fa fa-bars"></i>
+        </button>
         <div className="collapse navbar-collapse" id="nav-content">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
@@ -29,7 +33,7 @@ export default function NavigationBar({theme, setTheme}) {
       </div>
     </nav>
     ) : (
-    <nav className="navbar" data-bs-theme="dark" id="main-nav-bar">
+    <nav className="navbar box-shadow" data-bs-theme="dark" id="main-nav-bar">
       <div className="container-fluid d-flex justify-content-center">
         <a className="navbar-brand d-flex justify-content-center align-items-center gap-3" href="#">
           <img src="/assets/images/logo/64.png"/>
