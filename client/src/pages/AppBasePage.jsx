0f9887e88fc6770a59d0e63ed5fdff5d4325731f
elmfrain <elmfer10@gmail.com>
2024-01-24 19:17:15 -0600

    redirect user to admin login page if not logged in

--- client/src/pages/AppBasePage.jsx
+++ client/src/pages/AppBasePage.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
+import AuthService from "../utils/auth";
 
 export default function AppBasePage({theme, setTheme})
 {
+  if(!AuthService.isLoggedIn()) 
+    window.location.assign('/');
+
   const scrollDivStyle = {
     overflowY: "auto"
   };
