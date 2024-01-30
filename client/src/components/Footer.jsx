b0a921ced04c63699cdd62f05571985f8d8e92cc
elmfrain <elmfer10@gmail.com>
2024-01-30 00:39:03 -0600

    add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer({theme, setTheme}) {
+  return (
+    <footer className="navbar p-4 bg-dark box-shadow justify-content-center" data-bs-theme="dark">
+      <a className="navbar-brand d-flex justify-content-center align-items-center gap-3" href="#">
+        <img src="/assets/images/logo/64.png"/>
+        <div className="d-flex flex-column">
+          Attendance Portal
+          <span className="fs-6 text-secondary">v1.2.3</span>
+        </div>
+      </a>
+    </footer>
+  );
+}
--- client/src/pages/AppBasePage.jsx
+++ client/src/pages/AppBasePage.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import AuthService from "../utils/auth";
+import Footer from "../components/Footer";
 
 export default function AppBasePage({theme, setTheme})
 {
@@ -16,7 +17,9 @@ export default function AppBasePage({theme, setTheme})
       <NavigationBar theme={theme} setTheme={setTheme} />
       <div id="main-div" className="container flex-grow-1">
         <Outlet />
+        <div style={ {height: "0.5in"} } />
       </div>
+      <Footer />
     </div>
   )
 }
