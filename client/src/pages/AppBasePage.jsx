b04c59afbb95bbc4b37b767fa2d03901d7c00840
elmfrain <elmfer10@gmail.com>
2024-01-30 01:51:16 -0600

    scroll mainly in the body/html element, not the div

--- client/src/pages/AppBasePage.jsx
+++ client/src/pages/AppBasePage.jsx
@@ -8,16 +8,11 @@ export default function AppBasePage({theme, setTheme})
   if(!AuthService.isLoggedIn()) 
     window.location.assign('/');
 
-  const scrollDivStyle = {
-    overflowY: "auto"
-  };
-
   return (
-    <div className="d-flex flex-column gap-3 vh-100" style={scrollDivStyle}>
+    <div className="d-flex flex-column gap-3" style={{ minHeight: "100vh", height: "100%" }}>
       <NavigationBar theme={theme} setTheme={setTheme} />
       <div id="main-div" className="container flex-grow-1">
         <Outlet />
-        <div style={ {height: "0.5in"} } />
       </div>
       <Footer />
     </div>
